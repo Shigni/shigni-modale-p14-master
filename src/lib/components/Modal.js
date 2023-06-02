f2bbcc499b9c50b33e7c67fc87243f5c65a69604
shigni <louisservant86@gmail.com>
2023-06-02 17:08:35 +0200

    Added background

--- src/lib/components/Modal.js
+++ src/lib/components/Modal.js
@@ -6,10 +6,12 @@ function Modal({id, showModal, closeModal, parameter, message}) {
         <>
             {
                 showModal === true &&
-                <div className="modal" id={id} style={parameter}>
-                    <div className="modal-content">
-                        <span className="modal-close-icon" onClick={closeModal}>✖</span>
-                        <span>{message}</span>
+                <div className="modal-bg">
+                    <div className="modal" id={id} style={parameter}>
+                        <div className="modal-content">
+                            <span className="modal-close-icon" onClick={closeModal}>✖</span>
+                            <span>{message}</span>
+                        </div>
                     </div>
                 </div>
             }
